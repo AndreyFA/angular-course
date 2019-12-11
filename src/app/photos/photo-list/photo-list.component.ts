9070ad0bdd12d6d486e1e78e8e6f7ce61ed40c88
Andrey Frazatti Alves <andrey.alves@tokstok.com.br>
2019-12-11 19:49:14 -0300

    Página de NotFound

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -11,12 +13,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
 
+    const username = this.activatedRoute.snapshot.params.username;
+
     this.photoService
-          .listFromUser('flavio')
+          .listFromUser(username)
           .subscribe(photos => this.photos = photos);
   }
 }
