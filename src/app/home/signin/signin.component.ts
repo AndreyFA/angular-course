91031668d4647f90ea59ccce105668e25c94124d
Andrey Frazatti Alves <andrey.alves@tokstok.com.br>
2019-12-13 08:31:57 -0300

    Inicio da parte 2 do curso

---
+++ src/app/home/signin/signin.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+
+@Component({
+  templateUrl: 'signin.component.html'
+})
+export class SignInComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(username, password)
+      .subscribe(
+        () => {
+          console.log('autenticado');
+        },
+        error => {
+          console.log(error);
+          this.loginForm.reset();
+        });
+  }
+}
