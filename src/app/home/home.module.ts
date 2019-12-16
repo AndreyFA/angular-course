0781e0161ec8c4ad2845ff54a0d3dd591f8f9a9f
Andrey Frazatti Alves <andrey.alves@tokstok.com.br>
2019-12-16 19:09:36 -0300

    Validadores

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [
+    SignInComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     VMessageModule,
     RouterModule
